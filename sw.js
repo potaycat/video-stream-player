407112806da313f96b2274a83543f7cdd2876cdc
long_nh <long_nh@flinters.vn>
2022-09-26 17:13:11 +0700

    UX

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const cacheName = 'vsp-v1'
+const cacheName = 'vsp-v1.1'
 const contentToCache = [
     'index.html',
     'assets/icons/icon-72x72.png',
@@ -22,10 +22,10 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
     e.respondWith((async () => {
-        const r = await caches.match(e.request)
-        if (r) {
-            return r
-        }
+        // const r = await caches.match(e.request)
+        // if (r) {
+        //     return r
+        // }
         const response = await fetch(e.request)
         const cache = await caches.open(cacheName)
         cache.put(e.request, response.clone())
